aba2e5448055289162da49a216d018c9c7e3ed04
Itamar Fellus <itamar.fellus@gmail.com>
2019-11-28 19:22:39 +0200

    Fixed top-bar to the top of the screen

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -4,11 +4,7 @@ import { ButtonAppBar } from './MuiAppBar';
 
 export const TopBar = (p) => {
     return (
-<<<<<<< HEAD:src/AppBar.tsx
-        <div id="topBar">
-=======
         <div id="top-bar">
->>>>>>> 04ecc0d8a6dbce49b9831a49742da49a7ab7da5e:src/AppBar.jsx
             <div >
                 <ButtonAppBar />
             </div>

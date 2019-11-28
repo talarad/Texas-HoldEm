b503c0ea8477285d1041c3f889dfe5e03872f97a
Tal Arad <talarad5@gmail.com>
2019-11-28 17:26:15 +0200

    added cards images and cards on table logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React, { useState, useEffect, useRef } from 'react';
 import { SignIn } from './SignIn';
 import './App.css';
 import { TemporaryDrawer } from './drawer';
+import cards from './cards'
+
+let deck = shuffleArray(cards);
+
+function shuffleArray(cardsDeck: {}[]) {
+  for (let i = cardsDeck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [cardsDeck[i], cardsDeck[j]] = [cardsDeck[j], cardsDeck[i]];
+
+  }
+
+  return cardsDeck;
+}
 
 const App: React.FC = () => {
   const [username, setUsername] = useState<string | undefined>(undefined);
   const usernameRef = useRef<HTMLInputElement>()
   const passwordRef = useRef<HTMLInputElement>()
+  const [cardsInBoard, updateCardsInBoard] = useState<any[]>([])
+
+
+  function renderCards() {
+    return cardsInBoard.map((card, index) => {
+      const cardName = `${card.number}${card.kind[0]}`
+      return <div className="card"  key={cardName}> <img src={require(`./images/${cardName}.jpg`)} width="100px" height="150px" /></div >
+    })
+  }
 
   function signIn() {
     let usernameSignin: string;
@@ -25,7 +47,7 @@ const App: React.FC = () => {
         },
         body: JSON.stringify({ usernameSignin, passwordSignin })
       }).then(res => res.json()).then(res => {
-        if(res.status) {
+        if (res.status) {
           setUsername(usernameSignin);
         }
       })
@@ -34,17 +56,28 @@ const App: React.FC = () => {
     }
   }
 
+  function addRandomCard() {
+    const newDeck = [...cardsInBoard]
+    const randomNum = Math.floor(Math.random()*(deck.length));
+    newDeck.push(deck[randomNum]);
+    deck.splice(randomNum, 1)
+
+    updateCardsInBoard(newDeck)
+  }
+
   if (!username) {
     return <SignIn signIn={signIn} usernameRef={usernameRef} passwordRef={passwordRef} />
   } else {
     return (
       <div className="App">
-        <div className="App">
         <TemporaryDrawer />
-        <div className="bottom-table" />
-      </div> 
-        <div className="bottom-table" />
-      </div>
+        <div className="bottom-table">
+          <div>
+            <button onClick={addRandomCard}>ADD CARD</button>
+          </div>
+          {renderCards()}
+        </div>
+      </div >
     );
   }
 }
